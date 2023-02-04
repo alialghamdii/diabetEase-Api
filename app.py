e0839caafef9ad8ad18d29df901bf0400659b49a
h47311aa <ali.alghamdi-3@student.manchester.ac.uk>
2023-02-04 18:14:10 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+import os
+import csv
+import jsonpickle
+from model import predict
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome to the API."
+
+@app.route("/send_csv", methods=["POST"])
+def send_csv():
+    file = request.files['file']
+    file.save(os.path.join("csv_files", file.filename))
+    return "File received and saved successfully."
+
+@app.route("/get_csv", methods=["GET"])
+def get_csv():
+    csv_files = os.listdir("csv_files")
+    if len(csv_files) == 0:
+        return "No files found."
+    else:
+        latest_file = max(csv_files, key=lambda x: os.path.getctime("csv_files/" + x))
+        with open("csv_files/" + latest_file) as f:
+            reader = csv.reader(f)
+            data = [row for row in reader]
+            predictions = predict("csv_files/" + latest_file)
+        return jsonpickle.encode(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
