fc38594c5da930b4071161f92039562c9bbb4fd4
h47311aa <ali.alghamdi-3@student.manchester.ac.uk>
2023-02-05 01:16:04 +0000

    a

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 
 def predict(filename):
-    prediction_data = filename.to_csv("prediction_data.csv", encoding='utf-8', index=False)
+    df = pd.read_csv(filename, encoding='latin-1', header = None, names = columns)
+    prediction_data = df.to_csv("prediction_data.csv", encoding='utf-8', index=False)
     return prediction_data
